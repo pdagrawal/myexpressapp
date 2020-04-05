bc4a8ee610bc13b3e0e9cc8b61eb6a029140bfef
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-04-06 01:55:01 +0530

    printing message

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
- const http = require('http');
- const express = require('express');
+const http = require('http');
 
- const server = http.createServer();
- server.listen(3000);
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from express</h1>');
+});
+
+const server = http.createServer(app);
+server.listen(3000);
