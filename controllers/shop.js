6adc5db3b5823b5ba184a95d98e30a28bf4f29e4
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2021-06-07 23:33:18 +0530

    Added product-detail page

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,9 +23,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.id;
   Product.findById(prodId, product => {
-    console.log(product);
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
   });
-  res.redirect('/');
 }
 
 exports.getCart = (req, res, next) => {
