ea855b4d5d7b07fc23f33c81cb28d3eefde81638
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2021-06-08 00:23:31 +0530

    Removed logging

--- models/cart.js
+++ models/cart.js
@@ -17,19 +17,15 @@ module.exports = class Cart {
       }
       // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-      console.log(existingProductIndex);
       const existingProduct = cart.products[existingProductIndex];
-      console.log(existingProduct);
       // Add new product / increase quantity
       let updatedProduct;
       if (existingProduct) {
-        console.log('In if');
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
       } else {
-        console.log('In else');
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
